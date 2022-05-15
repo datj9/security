e84d9b9759dd0a1568379baa1d7f9697a9cd89c4
Dat Nguyen <tandat198@gmail.com>
2022-05-15 21:35:06 +0700

    add bcrypt, full helmet

---
+++ information-security/bcrypt-al.js
@@ -0,0 +1,7 @@
+// $2a$13$ZyprE5MRw2Q3WpNOGZWGbeG7ADUre1Q8QO.uUUtcbqloU0yvzavOm
+// $2a -> kind of hash algorithm
+// $13 -> cost -> how many times the data is put through the hasing algorithm, in this case
+// is 2^13 times
+
+// $ZyprE5MRw2Q3WpNOGZWGbeG7ADUre1Q8QO.uUUtcbqloU0yvzavOm
+// The first 22 characters is the salt in plain text, and the rest is the hashed password
--- information-security/helmet-lib.js
+++ information-security/helmet-lib.js
@@ -1,5 +1,54 @@
 const express = require("express")
 const helmet = require("helmet")
+const path = require("path")
 
+const PORT = 5001
 const app = express()
+app.use(express.static(__dirname + "/public"))
 app.use(helmet.hidePoweredBy())
+// does not allow another site use your site as a iframe
+app.use(
+  helmet.frameguard({
+    action: "DENY",
+  })
+)
+
+// turn off xssFilter because it still has some problems
+app.use(helmet.xssFilter())
+
+// disable browser to change the MIME Type of file
+// because it can cause
+app.use(helmet.noSniff())
+app.use(helmet.ieNoOpen())
+app.use(
+  helmet.hsts({
+    maxAge: 90 * 24 * 60 * 60,
+  })
+)
+app.use(helmet.dnsPrefetchControl())
+app.use(
+  helmet.contentSecurityPolicy({
+    directives: {
+      imgSrc: ["'self'", "www.w3schools.com"],
+    },
+  })
+)
+
+app.get("/", (_, res) =>
+  res.send(
+    `<div>Hello world <img src="https://www.w3schools.com/w3css/img_lights.jpg" /><img src="${"images/lights.jpeg"}" /></div>`
+  )
+)
+app.get("/with-iframe", (_, res) =>
+  res.send(`<div><iframe src="http://localhost:${PORT}" /></div>`)
+)
+app.get("/test", (req, res) => {
+  //   res.setHeader("Content-Type", "application/octet-stream")
+  //   res.setHeader(
+  //     "Content-Disposition",
+  //     'attachment; filename="new-test-file.html"'
+  //   )
+  return res.sendFile(path.join(__dirname, "public/templates/index.html"))
+})
+
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`))
